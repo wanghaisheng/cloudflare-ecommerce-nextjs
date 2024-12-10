9c5f87f0d58973f66f1675eb0baec768859a1315
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-09 16:34:32 -0800

    Get local dev working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+        },
+        {
+          protocol: "https",
+          hostname: "imagedelivery.net",
+        },
+      ],
+    },
+  };
 export default nextConfig;
--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -21,9 +21,9 @@ const Header: React.FC = () => {
               <input
                 type="text"
                 placeholder="Search products..."
-                className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none"
+                className="w-full pl-10 pr-4 py-2 rounded-lg border text-black border-gray-200 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:outline-none"
               />
-              <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+              <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-600" />
             </div>
           </div>
 
